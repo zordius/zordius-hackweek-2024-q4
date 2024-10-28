c82a375a7791cc664cf7bcf09cfdbfbf988af9a5
Zordius <1201409+zordius@users.noreply.github.com>
2024-10-28 15:22:55 +0900

    feat: size selector

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,45 @@
 require('../css/index.css');
 
+var step = 0;
+var size = 0;
+
+const setClass = () => {
+  document.body.className = `step${step} size${size}`;
+}
+
+const setFee = (fee1, fee2) => {
+  const fee = document.querySelectorAll('#fee span');
+  const range = document.querySelector('#fee i');
+  fee[0].style.setProperty('--num1', fee1);
+  range.innerHTML = fee2 ? '~' : '';
+  if (fee2) {
+    fee[1].style.setProperty('--num2', fee2);
+  }
+}
 
 const initApp = () => {
   document.body.innerHTML = `
-  <div id="fee"><span>1000</span>~<span>2000</span></div>
+  <div id="fee"><span></span><i></i><span></span></div>
+  <div id="size">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
   `;
-
+  document.querySelectorAll('#size span').forEach((span, i) => {
+    span.addEventListener('click', () => {
+      if (step === 1) {
+        size = 0;
+        step = 0;
+      } else {
+        size = i + 1;
+        step = 1;
+      }
+      setClass();
+    });
+  });
   document.body.classList.remove('loading');
+  setFee(160, 2000);
 }
 
 setTimeout(initApp, 0);
