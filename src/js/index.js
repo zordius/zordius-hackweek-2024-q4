8de6d11c8fbf476a9704ce6b4fe533b534562fa0
Zordius <1201409+zordius@users.noreply.github.com>
2024-10-28 15:54:16 +0900

    feat: add weight hint

--- src/js/index.js
+++ src/js/index.js
@@ -7,26 +7,28 @@ const setClass = () => {
   document.body.className = `step${step} size${size}`;
   switch (step) {
     case 1:
+    case 2:
       switch (size) {
         case 1:
-          return setFee(160);
+          return setFeeWeight(160, 0, 1);
         case 2:
-          return setFee(455);
+          return setFeeWeight(455, 0, 2);
         case 3:
-          return setFee(750, 1900);
+          return setFeeWeight(750, 1900, 25);
       }
     default:
-      setFee(160, 2000);
+      setFeeWeight(160, 2000);
   }
 }
 
-const setFee = (fee1, fee2) => {
+const setFeeWeight = (fee1, fee2, maxWeight) => {
   const fee = document.querySelectorAll('#fee span');
   const range = document.querySelector('#fee i');
   fee[0].style.setProperty('--num1', fee1);
   range.innerHTML = fee2 ? '~' : '';
   fee[1].style.setProperty('--num2', fee2);
   fee[1].className = fee2 ? '' : 'hide';
+  document.querySelector('#hint1 span').innerHTML = maxWeight;
 }
 
 const initApp = () => {
@@ -37,10 +39,11 @@ const initApp = () => {
     <span></span>
     <span></span>
   </div>
+  <div id="hint1">重さが<span></span>KG以下であることを確認してください。</div>
   `;
   document.querySelectorAll('#size span').forEach((span, i) => {
     span.addEventListener('click', () => {
-      if (step === 1) {
+      if (step !== 0) {
         size = 0;
         step = 0;
       } else {
@@ -49,6 +52,10 @@ const initApp = () => {
       }
       setClass();
     });
+  });
+  document.querySelector('#hint1').addEventListener('click', () => {
+    step = 2;
+    setClass();
   });
   setClass();
 }
