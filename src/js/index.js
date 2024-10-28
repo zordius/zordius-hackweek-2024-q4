43f03c5f93bb24a842297256ba3eba8afd035343
Zordius <1201409+zordius@users.noreply.github.com>
2024-10-28 12:03:36 +0900

    chore: loading css

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 require('../css/index.css');
 
-console.log('OK!');
+
+const initApp = () => {
+  document.body.innerHTML = `
+  <div id="fee"><span>1000</span>~<span>2000</span></div>
+  `;
+
+  document.body.classList.remove('loading');
+}
+
+setTimeout(initApp, 0);
